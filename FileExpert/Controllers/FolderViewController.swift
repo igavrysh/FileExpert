5a23652aad91cdd36946d93fe5bb034e48a659f3
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-05-29 02:51:13 -0700

    refactor icons flow layout

--- FileExpert/Controllers/FolderViewController.swift
+++ FileExpert/Controllers/FolderViewController.swift
@@ -8,24 +8,22 @@
 import UIKit
 import Foundation
 
-enum FolderStyle {
-    case list
+enum FolderStyle: Int {
     case icons
+    case list
 }
 
 class FolderViewController: BaseListController,
                             UICollectionViewDelegateFlowLayout
 {
     var style: FolderStyle
+    
     let sheetService = SheetService()
     var directory: Directory? = nil
     
     let cellId = "id"
-    let sectionInsets = UIEdgeInsets(top: 4.0, left: 4.0, bottom: 4.0, right: 4.0)
-    let numberOfItemsPerRow: CGFloat = 3.0
-    let spacingBetweenCells: CGFloat = 4.0
     
-    let simpleOver = SimpleOver()
+    var flows: [UICollectionViewDelegateFlowLayout] = [IconsFlowLayout()]
     
     let activityIndicatorView: UIActivityIndicatorView = {
         let aiv = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
@@ -46,7 +44,7 @@ class FolderViewController: BaseListController,
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         self.view.backgroundColor = .blue
         collectionView.register(IconViewCell.self, forCellWithReuseIdentifier: cellId)
         collectionView.backgroundColor = .white
@@ -117,7 +115,7 @@ class FolderViewController: BaseListController,
     }
     
     override func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        if let cell = collectionView.cellForItem(at: indexPath) as? IconViewCell {
+        if let cell = collectionView.cellForItem(at: indexPath) as? ItemView {
             cell.unhighlight()
         }
     }
@@ -126,7 +124,7 @@ class FolderViewController: BaseListController,
         _ collectionView: UICollectionView,
         cellForItemAt indexPath: IndexPath
     ) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! IconViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ItemView
         directory
             .flatMap({ $0.item(at: indexPath.item) })
             .map({ cell.updateUIWithItem($0) })
@@ -135,46 +133,6 @@ class FolderViewController: BaseListController,
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.directory?.itemsCount() ?? 0
-    }
-    
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout,
-        sizeForItemAt indexPath: IndexPath
-    ) -> CGSize {
-        let totalSpacing = (2 * sectionInsets.left)
-            + ((numberOfItemsPerRow - 1) * spacingBetweenCells) //Amount of total spacing in a row
-
-        if let collection = self.collectionView{
-            let width = (collection.bounds.width - totalSpacing) / numberOfItemsPerRow
-            return CGSize(width: width, height: width)
-        } else {
-            return CGSize(width: 0, height: 0)
-        }
-    }
-
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout,
-        insetForSectionAt section: Int
-    ) -> UIEdgeInsets {
-        return sectionInsets
-    }
-
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout,
-        minimumLineSpacingForSectionAt section: Int
-    ) -> CGFloat {
-        return spacingBetweenCells
-    }
-    
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout,
-        minimumInteritemSpacingForSectionAt section: Int
-    ) -> CGFloat {
-        return 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
@@ -187,5 +145,69 @@ class FolderViewController: BaseListController,
         if let cell = collectionView.cellForItem(at: indexPath) as? IconViewCell {
             cell.unhighlight()
         }
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        guard let size = currentFlow().collectionView?(
+                collectionView,
+                layout: collectionViewLayout,
+                sizeForItemAt: indexPath
+        ) else {
+            return CGSize.zero
+        }
+        return size
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        guard let insets = currentFlow().collectionView?(
+                collectionView,
+                layout: collectionViewLayout,
+                insetForSectionAt: section
+        ) else {
+            return UIEdgeInsets.zero
+        }
+        return insets
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        guard let spacing = currentFlow().collectionView?(
+                collectionView,
+                layout: collectionViewLayout,
+                minimumLineSpacingForSectionAt: section
+        ) else {
+            return CGFloat.zero
+        }
+        return spacing
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        guard let spacing = currentFlow().collectionView?(
+                collectionView,
+                layout: collectionViewLayout,
+                minimumInteritemSpacingForSectionAt: section
+        ) else {
+            return CGFloat.zero
+        }
+        return spacing
+    }
+    
+    func currentFlow() -> UICollectionViewDelegateFlowLayout {
+        return flows[style.rawValue]
     }
 }
