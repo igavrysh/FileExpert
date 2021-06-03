81bf399ed169d6840623d651c9795f1636a10bb1
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-06-02 18:08:15 -0700

    add ipad, iphone se, correct icon sizing

--- FileExpert/Models/Store.swift
+++ FileExpert/Models/Store.swift
@@ -24,8 +24,15 @@ class Store {
     
     func load() {
         DispatchQueue.global(qos: .background).async { [weak self] in
-            self?.rootFolder.add(Folder(name: "abc", id: "abc"))
             /*
+            self?.rootFolder.add(Folder(name: "abc", id: "abc"))
+            self?.rootFolder.add(
+                FileNew(
+                    name: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                    id: "id2"))
+            
+             
+            */
             self?.sheetService.fetchSheet { [weak self] (sheet, error) in
                 if error != nil {
                     return
@@ -34,7 +41,8 @@ class Store {
                     self?.loadDirectoryFromSheet(sheet)
                 }
             }
-            */
+            
+            
         }
         
     }
--- FileExpert/Views/IconViewCell.swift
+++ FileExpert/Views/IconViewCell.swift
@@ -11,44 +11,34 @@ class IconViewCell: UICollectionViewCell, ItemView {
    
     static let reuseIdentifier = "icon-view-cell-reuse-identifier"
     
-    let iconCellFillRatio: CGFloat = 0.60
+    static let testing = false
+    
+    let cellFillRatioIcon: CGFloat = 0.60
+    let cellFillRatioText: CGFloat = 0.30
+    
     let iconView: UIImageView = {
         var i = UIImageView()
-        i.backgroundColor = .blue
+        if IconViewCell.testing {
+            i.backgroundColor = .blue
+        }
+        i.contentMode = .scaleAspectFill
         return i
     }()
-    let iconViewContainer: UIView = {
-        var v = UIView()
-        v.backgroundColor = .yellow
-        return v
-    }()
     var iconSize: CGFloat = 0
-    var iconWidthConstraint: NSLayoutConstraint? = nil
-    var iconHeightConstraint: NSLayoutConstraint? = nil
     var item: Item? = nil
 
     let nameLabel: UILabel = {
         var l = UILabel(text: "hello", font: .systemFont(ofSize: 12), numberOfLines: 2)
         l.textAlignment = .center
         l.lineBreakMode = .byTruncatingMiddle
-        l.backgroundColor = .green
+        if IconViewCell.testing {
+            l.backgroundColor = .green
+        }
         return l
     }()
     
     func setIconImage(_ image: UIImage) {
-        let ratio = image.size.width / image.size.height
-        var width: CGFloat = 0
-        var height: CGFloat = 0
-        if ratio > 1 {
-            width = iconSize
-            height = iconSize / ratio
-        } else {
-            height = iconSize
-            width = iconSize * ratio
-        }
         self.iconView.image = image
-        _ = self.iconWidthConstraint.map { $0.constant = width }
-        _ = self.iconHeightConstraint.map { $0.constant = height}
     }
     
     func highlight() {
@@ -84,7 +74,7 @@ class IconViewCell: UICollectionViewCell, ItemView {
         } else {
             iconImage = UIImage(systemName: "doc.richtext")!
         }
-        //iconImage.map { setIconImage($0) }
+        iconImage.map { setIconImage($0) }
     }
     
     required init?(coder: NSCoder) {
@@ -94,61 +84,70 @@ class IconViewCell: UICollectionViewCell, ItemView {
 
 extension IconViewCell {
     func configureHierarchy() {
-        iconSize = frame.height * iconCellFillRatio
+        iconSize = frame.height * cellFillRatioIcon
         self.backgroundView = {
             let v = UIView()
             v.fillSuperview()
-            v.backgroundColor = .red
+            if IconViewCell.testing {
+                v.backgroundColor = .red
+            } else {
+                v.backgroundColor = .white
+            }
             v.layer.cornerRadius = 8
-            v.layer.shouldRasterize = true
             v.layer.borderWidth = 0.3
             v.layer.borderColor = UIColor.lightGray.cgColor
             self.addSubview(v)
             return v
         }()
+
         self.translatesAutoresizingMaskIntoConstraints = false
         
-        // remainig size after icon size is taken into consideration
-        let remainingSize = frame.height * (1 - iconCellFillRatio)
-        // H:|padding-[imageview]-padding-[label]-padding|
-        let padding = remainingSize * 0.4 / 3
-        // divide by 2 cos max two lines are allowed in item name label
-        let textHeight = remainingSize * 0.6 / 2
-        let fontSize = self.nameLabel.fontSizeToFitSize(CGSize(width: frame.width - padding * 2, height: textHeight))
-        nameLabel.font = .systemFont(ofSize: 3)
-        nameLabel.text = "placeholder"
+        let paddingViewGenerator = {() -> UIView in
+            let p = UIView()
+            p.translatesAutoresizingMaskIntoConstraints = false
+            if IconViewCell.testing {
+                p.backgroundColor = .green
+            }
+            return p
+        }
         
+        nameLabel.text = "placeholder"
+        let paddingToViewMul = (1 - cellFillRatioIcon - cellFillRatioText) / 5
+        
+        let paddingViewTop = paddingViewGenerator()
+        let paddingViewMiddle = paddingViewGenerator()
+    
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         iconView.translatesAutoresizingMaskIntoConstraints = false
-        iconViewContainer.translatesAutoresizingMaskIntoConstraints = false
-        iconViewContainer.addSubview(iconView)
-        self.addSubview(iconViewContainer)
-        //self.addSubview(nameLabel)
+        
+        self.addSubview(paddingViewTop)
+        self.addSubview(iconView)
+        self.addSubview(paddingViewMiddle)
+        self.addSubview(nameLabel)
+        
+        // top padding view setup
+        paddingViewTop.widthAnchor.constraint(equalToConstant: 0).isActive = true
+        paddingViewTop.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: paddingToViewMul * 3).isActive = true
+        paddingViewTop.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        paddingViewTop.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         
         // icon View constraints
-        iconHeightConstraint = self.iconView.constrainHeight(constant: iconSize)
-        iconWidthConstraint = self.iconView.constrainWidth(constant: iconSize)
+        iconView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: self.cellFillRatioIcon).isActive = true
+        iconView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: self.cellFillRatioIcon).isActive = true
+        iconView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        iconView.topAnchor.constraint(equalTo: paddingViewTop.bottomAnchor, constant: 0).isActive = true
         
-        iconView.centerXAnchor.constraint(equalTo: iconViewContainer.centerXAnchor).isActive = true
-        iconView.centerYAnchor.constraint(equalTo: iconViewContainer.centerYAnchor).isActive = true
-        iconWidthConstraint?.isActive = true
-        iconHeightConstraint?.isActive = true
-        
-        
-        iconViewContainer.widthAnchor.constraint(equalToConstant: iconSize).isActive = true
-        iconViewContainer.heightAnchor.constraint(equalToConstant: iconSize).isActive = true
-        iconViewContainer.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        iconViewContainer.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: padding).isActive = true
-        
-        /*
+        // middle padding view setup
+        paddingViewMiddle.widthAnchor.constraint(equalToConstant: 0).isActive = true
+        paddingViewMiddle.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: paddingToViewMul).isActive = true
+        paddingViewMiddle.topAnchor.constraint(equalTo: self.iconView.bottomAnchor).isActive = true
+        paddingViewMiddle.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         
         nameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    
-        nameLabel.topAnchor.constraint(equalTo: iconViewContainer.bottomAnchor, constant: padding).isActive = true
-        nameLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: padding).isActive = true
-        nameLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -padding).isActive = true
-         */
-    
+        nameLabel.topAnchor.constraint(equalTo: paddingViewMiddle.bottomAnchor).isActive = true
+        nameLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1 - 2 * paddingToViewMul).isActive = true
+        nameLabel.heightAnchor.constraint(lessThanOrEqualTo: self.heightAnchor, multiplier: cellFillRatioText).isActive = true
+        
         UIImage(named: "placeholder").map{ setIconImage($0) }
     }
 }
